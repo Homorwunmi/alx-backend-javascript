084201adaf469b8056d6f685aee503d3c5b9c134
Homorwunmi <omotayomowunmi@gmail.com>
2024-07-29 14:02:44 +0100

    Block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-   const tasknew = true;
-   const task2new = false;
+    const tasknew = true;
+    const task2new = false;
   }
 
   return [task, task2];
