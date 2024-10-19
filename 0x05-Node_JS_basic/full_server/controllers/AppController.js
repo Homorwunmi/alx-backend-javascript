2714d342074db53577ab708cd7a207f6b1b1682c
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-19 13:18:21 +0100

    The App cntroller

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+// Define and export the AppController class
+export default class AppController {
+    // Static method to handle the homepage route
+    static getHomepage(request, response) {
+      // Respond with a 200 status code and a message
+      response.status(200).send('Hello Holberton School!');
+    }
+  }
