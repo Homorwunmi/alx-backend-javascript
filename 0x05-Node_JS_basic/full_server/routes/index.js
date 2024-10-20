5347bd22d5c2b1136dc17d43d1d33d9283acfd9f
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-20 17:58:43 +0100

    Linking  the routes

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -4,15 +4,15 @@ import { Router } from 'express'; // Import express router
 import AppController from '../controllers/AppController'; // Import AppController class
 import StudentsController from '../controllers/StudentsController'; // Import StudentsController class
 
-const routes = Router(); // Initialize a new router instance
+const router = Router(); // Initialize a new router instance
 
 // Route for the homepage, handled by AppController
-routes.get('/', AppController.getHomepage);
+router.get('/', AppController.getHomepage);
 
 // Route for listing all students, handled by StudentsController
-routes.get('/students', StudentsController.getAllStudents);
+router.get('/students', StudentsController.getAllStudents);
 
 // Route for listing students by major, handled by StudentsController
-routes.get('/students/:major', StudentsController.getAllStudentsByMajor);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-export default routes; // Export the router instance
+export default router; // Export the router instance
