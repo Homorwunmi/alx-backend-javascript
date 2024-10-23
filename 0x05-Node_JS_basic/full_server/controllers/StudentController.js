0d671b4252be4837a25720aee61aa7af1c307e03
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-23 19:38:07 +0100

    Students controller with two static methods

--- 0x05-Node_JS_basic/full_server/controllers/StudentController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentController.js
@@ -3,48 +3,60 @@
 // Import the readDatabase function from the utils module
 import readDatabase from '../utils';
 
-// Define and export the StudentsController class
-export default class StudentsController {
-  // Static method to handle the /students route
-  static getAllStudents(request, response) {
-    const databaseFile = process.argv[2]; // Get the database file path from command line args
+// Define StudentsController class
+class StudentsController {
+  static getAllStudents(request, response, DATABASE) {
     // Call readDatabase with the database file path
-    readDatabase(databaseFile).then((studentsByField) => {
-      let response = 'This is the list of our students\n';
+    readDatabase(DATABASE)
+      .then((fields) => {
+        const students = [];
+        // let count = 0;
+        let msg;
 
-      // Sort fields alphabetically and build the response
-      Object.keys(studentsByField).sort().forEach((field) => {
-        response += `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}\n`;
+        // for (const key of Object.keys(fields)) {
+        //   count += fields[key].length;
+        // }
+
+        // students.push(`Number of students: ${count}`);
+        students.push('This is the list of our students');
+
+        for (const key of Object.keys(fields)) {
+          msg = `Number of students in ${key}: ${
+            fields[key].length
+          }. List: ${fields[key].join(', ')}`;
+
+          students.push(msg);
+        }
+        // Send the response with a 200 status code
+        response.send(200, `${students.join('\n')}`);
+      })
+      .catch(() => {
+        // Send a 500 status code with an error message if there's an issue loading the database
+        response.send(500, 'Cannot load the database');
       });
-
-      // Send the response with a 200 status code
-      response.status(200).send(response.trim());
-    }).catch(() => {
-      // Send a 500 status code with an error message if there's an issue loading the database
-      response.status(500).send('Cannot load the database');
-    });
   }
 
   // Static method to handle the /students/:major route
-  static getAllStudentsByMajor(request, response) {
-    const major = request.params.major; // Get the major from URL parameters
-    const databaseFile = process.argv[2]; // Get the database file path from command line args
+  static getAllStudentsByMajor(request, response, DATABASE) {
+    const { major } = request.params; // Get the major from URL parameters
 
     // Check if the major is either CS or SWE
     if (major !== 'CS' && major !== 'SWE') {
       // Send a 500 status code with an error message if the major is invalid
-      response.status(500).send('Major parameter must be CS or SWE');
-      return;
-    }
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      // Call readDatabase with the database file path
+      readDatabase(DATABASE)
+        .then((fields) => {
+          const students = fields[major];
 
-    // Call readDatabase with the database file path
-    readDatabase(databaseFile).then((studentsByField) => {
-      const students = studentsByField[major] || [];
-      // Send the response with a 200 status and the list of students
-      response.status(200).send(`List: ${students.join(', ')}`);
-    }).catch(() => {
-      // Send a 500 status code with an error message if there's an issue loading the database
-      response.status(500).send('Cannot load the database');
-    });
+          // Send the response with a 200 status and the list of students
+          response.send(200, `List: ${students.join(', ')}`);
+        })
+        // Send a 500 status code with an error message if there's an issue loading the database
+        .catch(() => response.send(500, 'Cannot load the database'));
+    }
   }
 }
+
+export default StudentsController;
