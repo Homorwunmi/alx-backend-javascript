6b6e7e252e270c0a0eef33f8768ae6aaa306a4d7
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-23 18:00:35 +0100

    Linking the routes

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,18 +1,27 @@
 /* eslint-disable */
 
-import { Router } from 'express'; // Import express router
+import express from 'express'; // Import express router
 import AppController from '../controllers/AppController'; // Import AppController class
 import StudentsController from '../controllers/StudentsController'; // Import StudentsController class
 
-const router = Router(); // Initialize a new router instance
-
-// Route for the homepage, handled by AppController
-router.get('/', AppController.getHomepage);
-
-// Route for listing all students, handled by StudentsController
-router.get('/students', StudentsController.getAllStudents);
-
-// Route for listing students by major, handled by StudentsController
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
-
-export default router; // Export the router instance
+function controllerRouting(app) {
+    const router = express.Router(); // Initialize a new router instance
+    app.use('/', router);
+  
+    // Route for the homepage, handled by AppController
+    router.get('/', (req, res) => {
+      AppController.getHomepage(req, res);
+    });
+  
+    // Route for listing all students, handled by StudentsController
+    router.get('/students', (req, res) => {
+      StudentsController.getAllStudents(req, res, process.argv[2]);
+    });
+  
+    // Route for listing students by major, handled by StudentsController
+    router.get('/students/:major', (req, res) => {
+      StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
+    });
+  }
+  
+  export default controllerRouting;
