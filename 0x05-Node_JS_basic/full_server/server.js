3cb104fcea4fa19a74db738a8f41ccaccaa0d269
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-23 17:56:17 +0100

    Express server

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 
 import express from 'express'; // Import the Express module
-import router from './routes/index'; // Import the routes from the index file
+import controllerRouting from './routes/index'; // Import the routes from the index file
 
 const app = express(); // Initialize an Express application instance
 
-// Use the imported routes for handling defined routes
-app.use('/', router);
-
 // Define the port number for the server to listen on
 const port = 1245;
+
+controllerRouting(app);
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    // console.log(`Server running on port ${port}`);
 });
 
 // Export the Express application instance for use in other modules
