ce3949425f6c08ca45fa3b472240020d3fc21dad
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-23 18:33:45 +0100

    The App controller

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,10 +1,11 @@
 /* eslint-disable */
 
-// Define and export the AppController class
-export default class AppController {
-    // Static method to handle the homepage route
-    static getHomepage(request, response) {
-      // Respond with a 200 status code and a message
-      response.status(200).send('Hello Holberton School!');
-    }
+class AppController {
+  // Static method to handle the homepage route
+  static getHomepage(request, response) {
+    // Respond with a 200 status code and a message
+    response.send(200, 'Hello Holberton School!');
   }
+}
+
+export default AppController;
